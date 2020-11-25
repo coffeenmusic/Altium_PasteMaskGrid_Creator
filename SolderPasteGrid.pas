ae0f5c42f519fbd38d3adeb9cd76bd65ee2a9451
Stephen Thompson <Stephen.Revel.Thompson@gmail.com>
2020-11-25 15:50:51 -0500

    Moved paste grid creation to separate function

--- SolderPasteGrid.pas
+++ SolderPasteGrid.pas
@@ -7,6 +7,9 @@
 { To use the script:                                                           }
 {  1/ Select the footprint's center pad.                                       }
 {  2/ Execute the script                                                       }
+{                                                                              }
+{ TODO:                                                                        }
+{  - Handle rotation                                                           }
 {..............................................................................}
 
 // Sets the paste mask expansion to a negative value less than the max pad
@@ -33,10 +36,82 @@ begin
     Pad.SetState_Cache := Padcache;
 end;
 
+function CreatePasteGrid(Board: IPCB_Board, Pad: IPCB_Primitive, Min_Grid_Size: Integer, Min_Gap: Integer);
+const
+    INTERN2MIL = 10000;  // Internal Unit Conversion to mils
+var
+    Grid_x_cnt, Grid_y_cnt                : Integer;
+    Grid_x_pad, Grid_y_pad                : Integer;
+    Pad_h, Pad_w                          : Double;
+    i, j                                  : Integer;
+    Fill                                  : IPCB_Fill;
+    fill_x1, fill_x2, fill_y1, fill_y2    : Double;
+    xorigin, yorigin                      : Double;
+begin
+    xorigin := Board.XOrigin;
+    yorigin := Board.YOrigin;
+
+    Pad_h := Pad.TopXSize;
+    Pad_w := Pad.TopYSize;
+
+    // Get grid counts
+    Grid_x_cnt := Floor((Pad_w/INTERN2MIL)/Min_Grid_Size);
+    Grid_y_cnt := Floor((Pad_h/INTERN2MIL)/Min_Grid_Size);
+
+    Grid_x_pad := 0; Grid_y_pad := 0;
+
+    While (Grid_x_pad < Min_Gap*INTERN2MIL) or (Grid_y_pad < Min_Gap*INTERN2MIL) Do
+    Begin
+         // Get Grid padding
+         Grid_x_pad := (Pad_w - (Grid_x_cnt*Min_Grid_Size*INTERN2MIL))/(Grid_x_cnt+1);
+         Grid_y_pad := (Pad_h - (Grid_y_cnt*Min_Grid_Size*INTERN2MIL))/(Grid_y_cnt+1);
+
+         If (Grid_x_pad < Min_Gap*INTERN2MIL) Then
+         Begin
+              Grid_x_cnt := Grid_x_cnt - 1;
+         End;
+         If (Grid_y_pad < Min_Gap*INTERN2MIL) Then
+         Begin
+              Grid_y_cnt := Grid_y_cnt - 1;
+         End;
+
+         // Error Exit
+         If Grid_x_cnt <= 0 Then Exit;
+         If Grid_y_cnt <= 0 Then Exit;
+    End;
+
+    For i := 0 To Grid_x_cnt - 1 Do
+    Begin
+         For j := 0 To Grid_y_cnt - 1 Do
+         Begin
+              // Create Fill
+              fill_x1 := Grid_x_pad*(i+1) + i*Min_Grid_Size*INTERN2MIL - (Pad_w/2);
+              fill_x2 := Grid_x_pad*(i+1) + (i+1)*Min_Grid_Size*INTERN2MIL - (Pad_w/2);
+              fill_y1 := Grid_y_pad*(j+1) + j*Min_Grid_Size*INTERN2MIL - (Pad_h/2);
+              fill_y2 := Grid_y_pad*(j+1) + (j+1)*Min_Grid_Size*INTERN2MIL - (Pad_h/2);
+
+              Fill := PCBServer.PCBObjectFactory(eFillObject, eNoDimension, eCreate_Default);
+              Fill.X1Location := fill_x1 + xorigin;
+              Fill.Y1Location := fill_y1 + yorigin;
+              Fill.X2Location := fill_x2 + xorigin;
+              Fill.Y2Location := fill_y2 + yorigin;
+              Fill.Layer := eTopPaste;
+              Fill.Rotation := 0;
+
+              // Add a new Fill into the PCB design database.
+              Board.AddPCBObject(Fill);
+         End;
+    End;
+
+    // Refresh the PCB document
+    ResetParameters;
+    AddStringParameter('Action', 'All');
+    RunProcess('PCB:Zoom');
+End;
+
 {..............................................................................}
 procedure SolderPasteGrid();
 const
-    INTERN2MIL = 10000;  // Internal Unit Conversion to mils
     MIN_PASTE_GRID = 40; // Minimum Grid Size in mils
     MIN_PASTE_GAP = 7;   // Minimum spacing between paste grids
 var
@@ -52,14 +127,8 @@ var
     Pad_R         : TPCBString;
     Pad_T         : TPCBString;
     Pad_B         : TPCBString;
-    Fill          : IPCB_Fill;
-    Grid_x_cnt    : Integer;
-    Grid_y_cnt    : Integer;
-    Grid_x_pad    : Integer;
-    Grid_y_pad    : Integer;
     xorigin, yorigin : Double;
-    i, j          : Integer;
-    fill_x1, fill_x2, fill_y1, fill_y2 : Double;
+
 begin
     // Retrieve the current board
     Board := PCBServer.GetCurrentPCBBoard;
@@ -93,65 +162,14 @@ begin
             //Pad_T := IntToStr(Pad_Rect.Top - yorigin);
             //Pad_B := IntToStr(Pad_Rect.Bottom - yorigin);
 
-            // Set Paste Mask Expansion To Remove Current Paste Mask
+
             If (Pad_Layer = 'Top Layer') and (Pad_x = 0) and (Pad_y = 0) Then
             Begin
+                 // Set Paste Mask Expansion To Remove Current Paste Mask
                  RemoveExistingPaste(Pad, Pad_w, Pad_h, Pad.PasteMaskExpansion);
+
+                 CreatePasteGrid(Board, Pad, MIN_PASTE_GRID, MIN_PASTE_GAP);
             End;
-
-            // Get grid counts
-            Grid_x_cnt := Floor((Pad_w/INTERN2MIL)/MIN_PASTE_GRID);
-            Grid_y_cnt := Floor((Pad_h/INTERN2MIL)/MIN_PASTE_GRID);
-
-            Grid_x_pad := 0; Grid_y_pad := 0;
-
-            While (Grid_x_pad < MIN_PASTE_GAP*INTERN2MIL) or (Grid_y_pad < MIN_PASTE_GAP*INTERN2MIL) Do
-            Begin
-                 // Get Grid padding
-                 Grid_x_pad := (Pad_w - (Grid_x_cnt*MIN_PASTE_GRID*INTERN2MIL))/(Grid_x_cnt+1);
-                 Grid_y_pad := (Pad_h - (Grid_y_cnt*MIN_PASTE_GRID*INTERN2MIL))/(Grid_y_cnt+1);
-
-                 If (Grid_x_pad < MIN_PASTE_GAP*INTERN2MIL) Then
-                 Begin
-                      Grid_x_cnt := Grid_x_cnt - 1;
-                 End;
-                 If (Grid_y_pad < MIN_PASTE_GAP*INTERN2MIL) Then
-                 Begin
-                      Grid_y_cnt := Grid_y_cnt - 1;
-                 End;
-
-                 // Error Exit
-                 If Grid_x_cnt <= 0 Then Exit;
-                 If Grid_y_cnt <= 0 Then Exit;
-            End;
-
-            FOR i := 0 TO Grid_x_cnt - 1 DO
-            BEGIN
-                 FOR j := 0 TO Grid_y_cnt - 1 DO
-                 BEGIN
-                     // Create Fill
-                     fill_x1 := Grid_x_pad*(i+1) + i*MIN_PASTE_GRID*INTERN2MIL - (Pad_w/2);
-                     fill_x2 := Grid_x_pad*(i+1) + (i+1)*MIN_PASTE_GRID*INTERN2MIL - (Pad_w/2);
-                     fill_y1 := Grid_y_pad*(j+1) + j*MIN_PASTE_GRID*INTERN2MIL - (Pad_h/2);
-                     fill_y2 := Grid_y_pad*(j+1) + (j+1)*MIN_PASTE_GRID*INTERN2MIL - (Pad_h/2);
-
-                     Fill := PCBServer.PCBObjectFactory(eFillObject, eNoDimension, eCreate_Default);
-                     Fill.X1Location := fill_x1 + xorigin;
-                     Fill.Y1Location := fill_y1 + yorigin;
-                     Fill.X2Location := fill_x2 + xorigin;
-                     Fill.Y2Location := fill_y2 + yorigin;
-                     Fill.Layer := eTopPaste;
-                     Fill.Rotation := 0;
-
-                     // Add a new Fill into the PCB design database.
-                     Board.AddPCBObject(Fill);
-                 END;
-            END;
-
-            // Refresh the PCB document
-            ResetParameters;
-            AddStringParameter('Action', 'All');
-            RunProcess('PCB:Zoom');
         End;
 
 
